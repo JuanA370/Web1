ef798c6e7db73406d154cde8df325ad6b539b690
Juan Antonio Castro Montero <118288650+JuanA370@users.noreply.github.com>
2026-09-16 17:25:12 +0200

    Archivo Js finalizado

--- Tema1-Prueba1/app.js
+++ Tema1-Prueba1/app.js
@@ -1,8 +1,7 @@
 //js del adivinador de numeros     
 
 let intentos = 0;
-const numeroSecreto = Math.floor(Math.random() * 100) + 1;
-console.log("(psst... el secreto es", numeroSecreto, ")");
+let numeroSecreto = Math.floor(Math.random() * 100) + 1;
 const listaNumeros = [];
 
 const inputNumero = document.getElementById("numero-input");
@@ -20,22 +19,34 @@ if (prediccion < 1 || prediccion > 100 || isNaN(prediccion)) {
     mensajePista.textContent = "Por favor, introduce un número entre 1 y 100.";
     return;
 }
-   intentos++;
-   listaNumeros.push(prediccion);
-   contadorIntentos.textContent = ` ${listaNumeros.join(", ")}`;
+    intentos++;
+    listaNumeros.push(prediccion);
+    contadorIntentos.textContent = intentos;
+    parrafoHistorial.textContent = `Has probado: ${listaNumeros.join(", ")}`;
 
-   if (prediccion === numeroSecreto) {
-       mensajePista.textContent = "Has adivinado el número.";
-       botonAdivinar.disabled = true;
-       reiniciarBtn.style.display = "inline-block";
+    if (prediccion === numeroSecreto) {
+        mensajePista.textContent = "Has adivinado el número.";
+        botonAdivinar.disabled = true;
+        reiniciarBtn.style.display = "inline-block";
 
-   }else if (intentos >= 7) {
+    }else if (intentos >= 7) {
     mensajePista.textContent = `Llegaste a 7 intentos, perdiste. El secreto era ${numeroSecreto}.`;
     botonAdivinar.disabled = true;
     reiniciarBtn.style.display = "inline-block";
-   }else if (prediccion < numeroSecreto) {
-         mensajePista.textContent = "El número secreto es mayor.";
+    }else if (prediccion < numeroSecreto) {
+        mensajePista.textContent = "El número secreto es mayor.";
     } else {
         mensajePista.textContent = "El número secreto es menor.";
     }
+});
+
+reiniciarBtn.addEventListener("click", () => {
+    numeroSecreto = Math.floor(Math.random() * 100) + 1;
+    intentos = 0;
+    listaNumeros.length = 0;
+    contadorIntentos.textContent = "0";
+    parrafoHistorial.textContent = "";
+    mensajePista.textContent = "";
+    botonAdivinar.disabled = false;
+    reiniciarBtn.style.display = "none";
 });
